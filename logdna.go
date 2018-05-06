8245dcbba46a5b1b8f6149380566e12afc6cbb42
Peter Cummings <peter@borealis-it.com>
2018-05-06 19:15:35 +0200

    Initial structure

---
+++ logdna.go
@@ -0,0 +1,124 @@
+package logdna
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	protocol = "https://"
+	domain   = "logs.logdna.com"
+	endpoint = "logs/ingest"
+)
+
+type LogdnaConfig struct {
+	IngestionKey string
+	LogLevel     []string
+	Tags         []string
+	AppName      string
+	Environment  string
+}
+type Client struct {
+	Config *LogdnaConfig
+
+	Response *Response
+}
+type Payload struct {
+	Line  string
+	App   string
+	Level string
+	Env   string
+}
+
+// NewClient returns a client for thechecker-go
+func NewClient(config *LogdnaConfig) (client *Client, err error) {
+	if config.IngestionKey == "" {
+		config.IngestionKey = os.Getenv("LOGDNA_KEY")
+	}
+	if config.IngestionKey == "" {
+		fmt.Println("LogDNA Ingestion Key not provided")
+		os.Exit(0)
+	}
+	client.Config = config
+	return client, nil
+}
+func (c *Client) Info(msg string) {
+	payload := c.configurePayload()
+	payload.Level = "INFO"
+	payload.Line = msg
+	if contains(c.Config.Tags, "INFO") {
+		c.do(payload)
+	}
+}
+func (c *Client) Notice(msg string) {
+	payload := c.configurePayload()
+	payload.Level = "NOTICE"
+	payload.Line = msg
+	if contains(c.Config.Tags, "NOTICE") {
+		c.do(payload)
+	}
+}
+func (c *Client) Warn(msg string) {
+	payload := c.configurePayload()
+	payload.Level = "WARN"
+	payload.Line = msg
+	if contains(c.Config.Tags, "WARN") {
+		c.do(payload)
+	}
+}
+func (c *Client) Error(msg string) {
+	payload := c.configurePayload()
+	payload.Level = "ERROR"
+	payload.Line = msg
+	if contains(c.Config.Tags, "ERROR") {
+		c.do(payload)
+	}
+}
+func (c *Client) Fatal(msg string) {
+	payload := c.configurePayload()
+	payload.Level = "FATAL"
+	payload.Line = msg
+	if contains(c.Config.Tags, "FATAL") {
+		c.do(payload)
+	}
+}
+func (c *Client) configurePayload() *Payload {
+	payload := new(Payload)
+	payload.App = c.Config.AppName
+	payload.Env = c.Config.Environment
+	return payload
+}
+func (c *Client) do(payload *Payload) {
+	u := &url.URL{
+		Scheme: protocol,
+		Host:   domain,
+		Path:   fmt.Sprintf("/%s", endPoint),
+	}
+	q := u.Query()
+	q.Set("hostname", getHostName())
+	q.Set("mac", getMacAddr())
+	q.Set("ip", getIpAddr())
+	q.Set("now", int32(time.Now().Unix()))
+	if len(c.Config.Tags) > 0 {
+		q.Set("tags", strings.Join(c.Config.Tags, ","))
+	}
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		//handle error
+	}
+	body := bytes.NewReader(payloadBytes)
+	r, err := http.NewRequest("POST", u.String(), body)
+	r.SetBasicAuth(c.Config.IngestionKey, "")
+	r.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		//handle error
+	}
+	defer Body.Close()
+}
---
+++ utils.go
@@ -0,0 +1,72 @@
+package logdna
+
+import (
+	"net"
+	"os"
+	"strings"
+)
+
+func getHostName() string {
+	name, err := os.Hostname()
+
+	if err != nil {
+		return err
+	}
+	return name
+}
+
+func getMacAddr() string {
+	addr := ""
+	interfaces, err := net.Interfaces()
+	if err == nil {
+		for _, i := range interfaces {
+			if i.Flags&net.FlagUp != 0 && bytes.Compare(i.HardwareAddr, nil) != 0 {
+				// Don't use random as we have a real address
+				addr = i.HardwareAddr.String()
+				break
+			}
+		}
+	}
+	return addr
+}
+func getIpAddr() string {
+	ip := ""
+	interfaces, err := net.Interfaces()
+	if err == nil {
+		for _, i := range interfaces {
+			if i.Flags&net.FlagUp != 0 && bytes.Compare(i.HardwareAddr, nil) != 0 {
+				addrs, err := i.Addrs()
+				if err != nil {
+					return err
+				}
+				for _, addr := range addrs {
+					var ip net.IP
+					switch v := addr.(type) {
+					case *net.IPNet:
+						ip = v.IP
+					case *net.IPAddr:
+						ip = v.IP
+					}
+					if ip == nil || ip.IsLoopback() {
+						continue
+					}
+					ip = ip.To4()
+					if ip == nil {
+						continue
+					}
+					ip = ip.String()
+				}
+				break
+			}
+		}
+	}
+	return ip
+}
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if strings.ToLower(a) == strings.ToLower(e) {
+			return true
+		}
+	}
+	return false
+}
